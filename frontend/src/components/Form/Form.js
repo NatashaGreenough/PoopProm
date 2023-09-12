e4a234b3dc1b509979755845045fd1b0e3100bb6
JuneKanyawat <kanyawat2002@gmail.com>
2023-09-12 16:46:27 +0700

    Query

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -3,6 +3,7 @@ import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
 import "./form-style.css";
+import "./queries.css";
 
 class Form extends Component {
   constructor(props) {
