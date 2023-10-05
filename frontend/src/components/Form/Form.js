e1d00a3d744bfd46d4c556515b7e337f42e30442
Apinut Mutasuwong <120626335+ApinutMutasuwong@users.noreply.github.com>
2023-10-06 00:33:07 +0700

    Update Form.js

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -88,7 +88,7 @@ class Form extends Component {
       console.log("Form submitted:", formData);
       console.log(images);
 
-      axios.post("http://54.166.210.15:5000/submit", formData);
+      axios.post("http://172.31.36.42:5000/submit", formData);
 
     } else {
       // Proceed to the next step
