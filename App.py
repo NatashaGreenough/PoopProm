9e7979f209bc8a5e629712cf07fd5525510edd2d
Apinut Mutasuwong <120626335+ApinutMutasuwong@users.noreply.github.com>
2023-10-05 20:50:59 +0700

    Update App.py

--- App.py
+++ App.py
@@ -16,6 +16,7 @@ db = mysql.connector.connect(
 
 def execute_query(query, data):
     try:
+        print("execute_query")
         cursor = db.cursor()
         cursor.execute(query, data)
         db.commit()
@@ -31,16 +32,19 @@ def execute_query(query, data):
             cursor.close()
 
 def nameDB(name):
+    print("nameDB")
     query = "INSERT INTO toilets(toilet_name) VALUES (%s)"
     data = (name,)
     return execute_query(query, data)
 
 def addressDB(address, district, province, zipcode, toiletID):
+    print("addressDB")
     query = "INSERT INTO address_info(toilet_address, toilet_district, toilet_province, toilet_zip, toilet_id) VALUES (%s, %s, %s, %s, %s)"
     data = (address, district, province, zipcode, toiletID)
     return execute_query(query, data)
     
 def labelDB(bidet, squat, auto, handicap, toiletID):
+    print("labelDB")
     query = "INSERT INTO label_info(bidet_spray, squat_toilet, auto_toilet, handicap_toilet, toilet_id) VALUES (%s, %s, %s, %s, %s)"
     data = (bidet, squat, auto, handicap, toiletID)
     return execute_query(query, data)
