f8c6a8462680787ae33df0f1a5b0ea6dddc5f09a
prabwong <prabwong@gmail.com>
2023-11-12 01:56:59 +0700

    Show map to pin location

--- frontend/src/components/Form/Step4.js
+++ frontend/src/components/Form/Step4.js
@@ -31,40 +31,41 @@ export default function Step4({ formData, handleChange }) {
   let pointLayer;
 
   useEffect(() => {
-    // Check if the script is already loaded
-    if (!window.nostra) {
-      console.log("Map already load");
-      const script = document.createElement("script");
-      script.src =
-        "//api.nostramap.com/nostraapi/v2.0?key=GDdAoW1Vs(48(1ni3MvBQyXGF(HgoA3Xy4LhYd1tJ1N)BwfaEO0k)lySSnEV9(gn2wfgYAToSXKYfCpy2DC9O60=====2";
-      script.async = true;
-      script.onload = initializeMap;
-      document.body.appendChild(script);
-
-      return () => {
-        document.body.removeChild(script);
-      };
-    } else {
-      // Nostra API is already loaded, directly initialize the map
-      console.log("Starrt new load");
-      initializeMap(); // Pass initial values here
+    const scriptSrc = "//api.nostramap.com/nostraapi/v2.0?key=GDdAoW1Vs(48(1ni3MvBQyXGF(HgoA3Xy4LhYd1tJ1N)BwfaEO0k)lySSnEV9(gn2wfgYAToSXKYfCpy2DC9O60=====2";
+  
+    // Remove existing script with the same source
+    const existingScript = document.querySelector(`script[src="${scriptSrc}"]`);
+    if (existingScript) {
+      document.body.removeChild(existingScript);
     }
-  },);
+  
+    // Create a new script element
+    const script = document.createElement("script");
+    script.src = scriptSrc;
+    script.async = true;
+    script.onload = initializeMap;
+  
+    // Append the new script to the body
+    document.body.appendChild(script);
+  
+    // Cleanup: remove the script when the component is unmounted
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   const initializeMap = () => {
-    // const mapContainer = document.getElementById("map");
+    const mapContainer = document.getElementById("map");
 
-    // console.log(formData);
-    // const key_word = formData.address + " " + formData.district + " " + formData.province + " " + formData.zipCode
-    // console.log(key_word);
-
-    searchAddressByKeyword();
+    console.log(formData);
+    const key_word = formData.address + " " + formData.district + " " + formData.province + " " + formData.zipCode
+    console.log(key_word);
     let map;
 
     nostra.onready = function () {
       map = new nostra.maps.Map("map", {
         id: "mapTest",
-        logo: true,
+        logo: false,
         scalebar: false,
         basemap: "streetmap",
         slider: true,
@@ -80,212 +81,223 @@ export default function Step4({ formData, handleChange }) {
       });
       map.addLayer(pointLayer);
 
+      console.log(formData.latitude);
+      console.log(formData.longitude);
+
       map.events.click = function (e) {
         handleMapClick(e);
       };
     };
-  };
-
-  const handleMapClick = (e) => {
-    const coordinates = e.mapPoint;
-    const clickedLatitude = coordinates.y;
-    const clickedLongitude = coordinates.x;
-
-    // console.log(clickedLatitude, clickedLongitude);
-
-    pointLayer.clear();
-
-    setLatitude(clickedLatitude.toFixed(8));
-    setLongitude(clickedLongitude.toFixed(8));
-
-    console.log(clickedLatitude.toFixed(8), clickedLongitude.toFixed(8));
-    // console.log(latitude, longitude);
-
-    handleChange({
-      target: { name: "latitude", value: clickedLatitude.toFixed(8) },
-    });
-    handleChange({
-      target: { name: "longitude", value: clickedLongitude.toFixed(8) },
-    });
-
-    addMarker(clickedLatitude, clickedLongitude);
-  };
+    const handleMapClick = (e) => {
+      const coordinates = e.mapPoint;
+      const clickedLatitude = coordinates.y;
+      const clickedLongitude = coordinates.x;
   
-  const addMarker = (latitude, longitude) => {
-    try {
-      const pointMarker = new nostra.maps.symbols.Marker({
-        url: "",
-        width: 60,
-        height: 60,
-        onClick: function () {
-          console.log("Marker clicked!");
-        },
+      // console.log(clickedLatitude, clickedLongitude);
+      pointLayer.clear();
+  
+      setLatitude(clickedLatitude.toFixed(8));
+      setLongitude(clickedLongitude.toFixed(8));
+  
+      console.log(clickedLatitude.toFixed(8), clickedLongitude.toFixed(8));
+      // console.log(latitude, longitude);
+  
+      handleChange({
+        target: { name: "latitude", value: clickedLatitude.toFixed(8) },
       });
-
-      pointLayer.addMarker(latitude, longitude, pointMarker);
-    } catch (error) {
-      console.error("Error adding marker:", error);
-    }
-  };
-
-  const searchAddressByKeyword = () => {
-    const keyword = formData.address + " " + formData.district + " " + formData.province + " " + formData.zipCode;
-    const numReturn = 1;
   
-    fetch(`http://api.nostramap.com/Service/V2/Location/addressSearch?key=GDdAoW1Vs(48(1ni3MvBQyXGF(HgoA3Xy4LhYd1tJ1N)BwfaEO0k)lySSnEV9(gn2wfgYAToSXKYfCpy2DC9O60=====2&keyword=${keyword}&numReturn=${numReturn}`)
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle the address search results
-        console.log("Address search results:", data);
-  
-        // Process the results as needed and update your component state
-        // Example: Update state with the first result
-        if (data.results && data.results.length > 0) {
-          const firstResult = data.results[0];
-          console.log("First result:", firstResult);
-          const locationID = firstResult.LocationID;
-          console.log(locationID)
-
-          if (locationID) {
-            getLocationDetails(locationID);
-          } else {
-            console.error("Location ID is undefined");
-          }
-  
-          // Update state or perform any other actions based on the result
-        }
-      })
-      .catch((error) => {
-        console.error("Address search error:", error);
+      handleChange({
+        target: { name: "longitude", value: clickedLongitude.toFixed(8) },
       });
-  };
-
-  const getLocationDetails = (locationID) => {
-    console.log(locationID)
-
-    const apiUrl = "http://api.nostramap.com/Service/V2/Location/addressSearchDetail?key=GDdAoW1Vs(48(1ni3MvBQyXGF(HgoA3Xy4LhYd1tJ1N)BwfaEO0k)lySSnEV9(gn2wfgYAToSXKYfCpy2DC9O60=====2&locationID=" + locationID + '&language=${"L"}'
+  
+      console.log(formData.latitude);
+      console.log(formData.longitude);
+  
+      addMarker(clickedLatitude, clickedLongitude);
+    };
     
-    console.log(apiUrl)
-
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle the location details
-        console.log("Location details1:", data);
-
-        // Process the details as needed and update your component state
-        // Example: Update state with the details
-        if (data.results) {
-          const locationDetails = data.results;
-          console.log("Location details2:", locationDetails);
-          const latLonString = locationDetails.LAT_LON;
-          console.log("Location latitude longitude:", locationDetails.LAT_LON);
-
-          if (latLonString) {
-            const [latitudeStart, longitudeStart] = latLonString.split(',').map(coord => parseFloat(coord.trim()));
-            console.log(latitudeStart, longitudeStart)
-            openeMap(latitudeStart, longitudeStart);
-            
-          }
-          
-        }
-      })
-      .catch((error) => {
-        console.error("Location details error:", error);
-      });
-
-  const openeMap = (latitudeStart, longitudeStart) => {
-    let map;
-    console.log("map open")
-
-    nostra.onready = function () {
-      map = new nostra.maps.Map("map", {
-        id: "mapTest",
-        logo: true,
-        scalebar: true,
-        basemap: "streetmap",
-        slider: true,
-        level: 18,
-        lat: 13.722944,
-        lon: 100.530449,
-        country: "TH",
-      });
-
-
-      pointLayer = new nostra.maps.layers.GraphicsLayer(map, {
-        id: "pointLayer",
-        mouseOver: false,
-      });
-      map.panTo(latitudeStart, longitudeStart);
-
-      map.addLayer(pointLayer);
-
-      setLatitude(latitudeStart.toFixed(8));
-      setLongitude(longitudeStart.toFixed(8));
-
-      handleChange({
-        target: { name: "latitude", value: latitudeStart.toFixed(8) },
-      });
-      handleChange({
-        target: { name: "longitude", value: longitudeStart.toFixed(8) },
-      });
-
-      console.log("Latitude change", latitude);
-      console.log("Longitude change", longitude);
-
-      map.events.click = function (e) {
-        handleMapClick(e);
-      };
+    const addMarker = (latitude, longitude) => {
+      try {
+        const pointMarker = new nostra.maps.symbols.Marker({
+          url: "",
+          width: 60,
+          height: 60,
+          onClick: function () {
+            console.log("Marker clicked!");
+          },
+        });
+  
+        pointLayer.addMarker(latitude, longitude, pointMarker);
+      } catch (error) {
+        console.error("Error adding marker:", error);
+      }
     };
   };
 
-  const handleMapClick = (e) => {
-    const coordinates = e.mapPoint;
-    const clickedLatitude = coordinates.y;
-    const clickedLongitude = coordinates.x;
-
-    // console.log(clickedLatitude, clickedLongitude);
-
-    pointLayer.clear();
-
-    setLatitude(clickedLatitude.toFixed(8));
-    setLongitude(clickedLongitude.toFixed(8));
-
-    console.log(clickedLatitude.toFixed(8), clickedLongitude.toFixed(8));
-    // console.log(latitude, longitude);
-
-    handleChange({
-      target: { name: "latitude", value: clickedLatitude.toFixed(8) },
-    });
-    handleChange({
-      target: { name: "longitude", value: clickedLongitude.toFixed(8) },
-    });
-
-    addMarker(clickedLatitude, clickedLongitude);
-  };
+  // const searchAddressByKeyword = () => {
+  //   const keyword = formData.address + " " + formData.district + " " + formData.province + " " + formData.zipCode;
+  //   const numReturn = 1;
   
-  const addMarker = (latitude, longitude) => {
-    try {
-      const pointMarker = new nostra.maps.symbols.Marker({
-        url: "",
-        width: 60,
-        height: 60,
-        onClick: function () {
-          console.log("Marker clicked!");
-        },
-      });
+  //   fetch(`http://api.nostramap.com/Service/V2/Location/addressSearch?key=GDdAoW1Vs(48(1ni3MvBQyXGF(HgoA3Xy4LhYd1tJ1N)BwfaEO0k)lySSnEV9(gn2wfgYAToSXKYfCpy2DC9O60=====2&keyword=${keyword}&numReturn=${numReturn}`)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // Handle the address search results
+  //       console.log("Address search results:", data);
+  
+  //       // Process the results as needed and update your component state
+  //       // Example: Update state with the first result
+  //       if (data.results && data.results.length > 0) {
+  //         const firstResult = data.results[0];
+  //         console.log("First result:", firstResult);
+  //         const locationID = firstResult.LocationID;
+  //         console.log(locationID)
 
-      pointLayer.addMarker(latitude, longitude, pointMarker);
-    } catch (error) {
-      console.error("Error adding marker:", error);
-    }
-  };
+  //         if (locationID) {
+  //           getLocationDetails(locationID);
+  //         } else {
+  //           console.error("Location ID is undefined");
+  //         }
+  
+  //         // Update state or perform any other actions based on the result
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error("Address search error:", error);
+  //     });
+      
+  // };
 
-  };
+  // const getLocationDetails = (locationID) => {
+  //   console.log(locationID)
 
+  //   const apiUrl = "http://api.nostramap.com/Service/V2/Location/addressSearchDetail?key=GDdAoW1Vs(48(1ni3MvBQyXGF(HgoA3Xy4LhYd1tJ1N)BwfaEO0k)lySSnEV9(gn2wfgYAToSXKYfCpy2DC9O60=====2&locationID=" + locationID + '&language=${"L"}'
+    
+  //   console.log(apiUrl)
+
+  //   fetch(apiUrl)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // Handle the location details
+  //       console.log("Location details1:", data);
+
+  //       // Process the details as needed and update your component state
+  //       // Example: Update state with the details
+  //       if (data.results) {
+  //         const locationDetails = data.results;
+  //         console.log("Location details2:", locationDetails);
+  //         const latLonString = locationDetails.LAT_LON;
+  //         console.log("Location latitude longitude:", locationDetails.LAT_LON);
+
+  //         if (latLonString) {
+  //           const [latitudeStart, longitudeStart] = latLonString.split(',').map(coord => parseFloat(coord.trim()));
+  //           console.log(latitudeStart, longitudeStart)
+  //           openeMap(latitudeStart, longitudeStart);
+            
+  //         }
+          
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error("Location details error:", error);
+  //     });
+
+  // const openeMap = (latitudeStart, longitudeStart) => {
+  //   let map;
+  //   console.log("map open")
+  //   console.log(latitudeStart, longitudeStart)
+    
+
+  //   nostra.onready = function () {
+  //     map = new nostra.maps.Map("map", {
+  //       id: "mapTest",
+  //       logo: true,
+  //       scalebar: true,
+  //       basemap: "streetmap",
+  //       slider: true,
+  //       level: 18,
+  //       lat: latitudeStart,
+  //       lon: longitudeStart,
+  //       country: "TH",
+  //     });
+
+
+  //   pointLayer = new nostra.maps.layers.GraphicsLayer(map, {
+  //     id: "pointLayer",
+  //     mouseOver: false,
+  //   });
+      
+  //     console.log("map close")
+
+  //     map.addLayer(pointLayer);
+
+  //     setLatitude(latitudeStart.toFixed(8));
+  //     setLongitude(longitudeStart.toFixed(8));
+
+  //     handleChange({
+  //       target: { name: "latitude", value: latitudeStart.toFixed(8) },
+  //     });
+
+  //     handleChange({
+  //       target: { name: "longitude", value: longitudeStart.toFixed(8) },
+  //     });
+
+  //     console.log("Latitude change", latitude);
+  //     console.log("Longitude change", longitude);
+
+  //     map.events.click = function (e) {
+  //       handleMapClick(e);
+  //     };
+  //   };
+  // };
+
+  // const handleMapClick = (e) => {
+  //   const coordinates = e.mapPoint;
+  //   const clickedLatitude = coordinates.y;
+  //   const clickedLongitude = coordinates.x;
+
+  //   // console.log(clickedLatitude, clickedLongitude);
+  //   pointLayer.clear();
+
+  //   setLatitude(clickedLatitude.toFixed(8));
+  //   setLongitude(clickedLongitude.toFixed(8));
+
+  //   console.log(clickedLatitude.toFixed(8), clickedLongitude.toFixed(8));
+  //   // console.log(latitude, longitude);
+
+  //   handleChange({
+  //     target: { name: "latitude", value: clickedLatitude.toFixed(8) },
+  //   });
+
+  //   handleChange({
+  //     target: { name: "longitude", value: clickedLongitude.toFixed(8) },
+  //   });
+
+  //   console.log(formData.latitude);
+  //   console.log(formData.longitude);
+
+  //   addMarker(clickedLatitude, clickedLongitude);
+  // };
+  
+  // const addMarker = (latitude, longitude) => {
+  //   try {
+  //     const pointMarker = new nostra.maps.symbols.Marker({
+  //       url: "",
+  //       width: 60,
+  //       height: 60,
+  //       onClick: function () {
+  //         console.log("Marker clicked!");
+  //       },
+  //     });
+
+  //     pointLayer.addMarker(latitude, longitude, pointMarker);
+  //   } catch (error) {
+  //     console.error("Error adding marker:", error);
+    // }
+  // };
+// };
   return (
     <div className="Toilet-info-container">
-      <div id="map" style={{ height: "600px", width: "600px" }}></div>
+      <div id="map" style={{ height: "600px", width: "768px" }}></div>
       <div></div>
       <div>
         <label>Latitude</label>
@@ -294,6 +306,7 @@ export default function Step4({ formData, handleChange }) {
           name="latitude"
           value={latitude}
           onChange={(e) => setLatitude(e.target.value)}
+          disabled
         />
         <label>Longitude</label>
         <input
@@ -301,6 +314,7 @@ export default function Step4({ formData, handleChange }) {
           name="longitude"
           value={longitude}
           onChange={(e) => setLongitude(e.target.value)}
+          disabled
         />
       </div>
     </div>
